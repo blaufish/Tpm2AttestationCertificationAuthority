b87f3020745b896467427522b28f86ecf84a6d73
blaufish <blaufish@users.noreply.github.com>
2018-08-16 16:41:34 +0200

    Nest junit tests

--- test/blaufish/test/tpm2/Tpm2AttestationCATest.java
+++ test/blaufish/test/tpm2/Tpm2AttestationCATest.java
@@ -8,43 +8,61 @@ import java.security.cert.X509Certificate;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class Tpm2AttestationCATest {
-	private static final String MANUFACTURER_ECC_CERT = "test/OptigaEccMfrCA022.crt";
-	private static final String MANUFACTURER_RSA_CERT = "test/OptigaRsaMfrCA022.crt";
-	private static final String ENDORSEMENT_KEY_ECC_CERT = "test/nvread.1c000a.cert";
-	private static final String ENDORSEMENT_KEY_RSA_CERT = "test/nvread.1c0002.cert";
-	Tpm2AttestationCA eccCa;
-	Tpm2AttestationCA rsaCa;
+
 	CertificateFactory certificateFactory;
+	Tpm2AttestationCA ca;
 
 	@BeforeEach
 	void before() throws Exception {
 		certificateFactory = CertificateFactory.getInstance("X.509");
-		X509Certificate manufacturerEccCertificate = loadCertificate(MANUFACTURER_ECC_CERT);
-		eccCa = Tpm2AttestationCA.build(manufacturerEccCertificate);
-		X509Certificate manufacturerRsaCertificate = loadCertificate(MANUFACTURER_RSA_CERT);
-		rsaCa = Tpm2AttestationCA.build(manufacturerRsaCertificate);
+		ca = null;
+	}
+
+	@AfterEach
+	void after() {
+		certificateFactory = null;
+		ca = null;
 	}
 
 	private X509Certificate loadCertificate(String certificateFile) throws CertificateException, FileNotFoundException {
 		return (X509Certificate) certificateFactory.generateCertificate(new FileInputStream(certificateFile));
 	}
 
-	@AfterEach
-	void after() {
-		eccCa = null;
-		rsaCa = null;
-		certificateFactory = null;
+	@Nested
+	class ecc {
+		private static final String MANUFACTURER_ECC_CERT = "test/OptigaEccMfrCA022.crt";
+		private static final String ENDORSEMENT_KEY_ECC_CERT = "test/nvread.1c000a.cert";
+
+		@BeforeEach
+		void before() throws Exception {
+			X509Certificate manufacturerEccCertificate = loadCertificate(MANUFACTURER_ECC_CERT);
+			ca = Tpm2AttestationCA.build(manufacturerEccCertificate);
+		}
+
+		@Test()
+		void testEKVerifyEcc() throws Exception {
+			ca.verifyEKCert(loadCertificate(ENDORSEMENT_KEY_ECC_CERT));
+		}
 	}
 
-	@Test()
-	void testEKVerifyEcc() throws Exception {
-		eccCa.verifyEKCert(loadCertificate(ENDORSEMENT_KEY_ECC_CERT));
-	}
-	@Test
-	void testEKVerifyRsa() throws Exception {
-		rsaCa.verifyEKCert(loadCertificate(ENDORSEMENT_KEY_RSA_CERT));
+	@Nested
+	class rsa {
+		private static final String MANUFACTURER_RSA_CERT = "test/OptigaRsaMfrCA022.crt";
+		private static final String ENDORSEMENT_KEY_RSA_CERT = "test/nvread.1c0002.cert";
+
+		@BeforeEach
+		void before() throws Exception {
+			X509Certificate manufacturerRsaCertificate = loadCertificate(MANUFACTURER_RSA_CERT);
+			ca = Tpm2AttestationCA.build(manufacturerRsaCertificate);
+		}
+
+		@Test
+		void testEKVerifyRsa() throws Exception {
+			ca.verifyEKCert(loadCertificate(ENDORSEMENT_KEY_RSA_CERT));
+		}
 	}
 }
